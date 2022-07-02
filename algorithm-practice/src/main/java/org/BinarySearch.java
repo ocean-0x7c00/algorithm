4b39ed752c1295d02067bd4e2fa75828e64cd33e
yancy <yancy@email.com>
2022-07-02 16:18:56 +0800

    二分查找之找峰值

--- algorithm-practice/src/main/java/org/BinarySearch.java
+++ algorithm-practice/src/main/java/org/BinarySearch.java
@@ -223,6 +223,47 @@ public class BinarySearch {
         return -1;
     }
 
+
+    /**
+     * 求峰值
+     * 峰值元素是指其值严格大于左右相邻值的元素。
+     * <p>
+     * 给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
+     * <p>
+     * 你可以假设 nums[-1] = nums[n] = -∞ 。
+     * <p>
+     * 你必须实现时间复杂度为 O(log n) 的算法来解决此问题。
+     */
+    public static int findPeakElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        if (nums.length == 1 || nums[0] > nums[1]) {
+            return 0;
+        }
+        if (nums[nums.length - 1] > nums[nums.length - 2]) {
+            return nums.length - 1;
+        }
+
+
+        int L = 1;
+        int R = nums.length - 2;
+
+        while (L < R) {
+            int mid = L + (R - L) / 2;
+            if (nums[mid] < nums[mid - 1]) {
+                R = mid - 1;
+            } else if (nums[mid] < nums[mid + 1]) {
+                L = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return L;
+    }
+
+
     // 二分查找 --- [left, right)
     // 数组已经是有序的了!
     int binarySearch2(int[] nums, int target) {
